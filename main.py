a2ce58ced36da009b56d8cbc2989e4572c21f8a0
Isaac-Rios12 <rios_joan12@yahoo.com>
2026-01-06 16:49:08 -0600

    use argparse for input follower files

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+import argparse
+
 from src.parser import parse_raw_followers
 from src.comparator import compare_followers
 
 from src.writer import write_user_list
 import os
 
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Instagram follower tracker"
+    )
+
+    parser.add_argument(
+        "day1",
+        help="Follower file from day 1"
+    )
+    parser.add_argument(
+        "day2",
+        help="Follower file from day 2"
+    )
+
+    return parser.parse_args()
 
 
-
-# followers = parse_raw_followers("data/raw/followers_day1.txt")
-# # print(followers)
-
-# print("usernames detectados: ")
-# for user in sorted(followers):
-#     print(user)
 
 def main():
+    args = get_args()
     #me aseguro que exista
     os.makedirs("output", exist_ok=True)
 
-    followers_day1 = parse_raw_followers("data/raw/followers_day1.txt")
-    followers_day2 = parse_raw_followers("data/raw/followers_day2.txt")
+    followers_day1 = parse_raw_followers(args.day1)
+    followers_day2 = parse_raw_followers(args.day2)
 
     unfollowers, new_followers = compare_followers(
         followers_day1,
